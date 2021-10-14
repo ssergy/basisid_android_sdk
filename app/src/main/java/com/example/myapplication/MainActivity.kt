0137dc61f3422af768f7536c37e1e529fa9050ad
sid <1sid1_86@mail.ru>
2021-10-14 14:41:04 +0300

    all

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.myapplication
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.sdk.basis.SdkBasisIDCallback
+import android.content.Intent
+import android.widget.EditText
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btn: Button = findViewById(R.id.button_start)
+        btn.setOnClickListener {
+            val cb = CallbackSdk()
+
+
+            var token: EditText = findViewById(R.id.token)
+
+            com.sdk.basis.SdkBasisID.initBasisID("", token.text.toString(), "europe", cb)
+            val intent = Intent(this, com.sdk.basis.SdkMainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    class CallbackSdk: SdkBasisIDCallback {
+        override fun send(status: String, code: String)     {
+            println("DEBUG callbackSdk status = $status  code = $code")
+        }
+    }
+}
