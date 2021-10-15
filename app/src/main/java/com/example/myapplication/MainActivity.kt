195048803e039511fffb0a42b57cb4290ff7c3aa
sid <1sid1_86@mail.ru>
2021-10-15 15:17:15 +0300

    fix

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -8,6 +8,7 @@ import com.sdk.basis.SdkBasisIDCallback
 import android.content.Intent
 
 
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,16 +16,36 @@ class MainActivity : AppCompatActivity() {
 
         val btn: Button = findViewById(R.id.button_start)
         btn.setOnClickListener {
-            val cb = CallbackSdk()
+            val cb = CallbackSdk(::goToEnd)
             com.sdk.basis.SdkBasisID.initBasisID("{API_key}", "{api_form_token}", "europe", cb)
             val intent = Intent(this, com.sdk.basis.SdkBasisIDMainActivity::class.java)
             startActivity(intent)
         }
     }
 
-    class CallbackSdk: SdkBasisIDCallback {
+    fun goToEnd() {
+        val intent = Intent(this, End::class.java)
+        startActivity(intent)
+    }
+
+    class CallbackSdk(val f: () -> Unit) : SdkBasisIDCallback {
         override fun send(status: String, code: String)     {
             println("DEBUG callbackSdk status = $status  code = $code")
+            if (status == "ok") {
+                when (code) {
+                    "finish" -> {
+                        f() // show your activity
+                    }
+                    "video" -> println("first step completed")
+                    "full" -> println("verification completed")
+                }
+            }else {
+                when (code) {
+                    "step_timeout" -> println("verification step timeout exceed")
+                    "manual_review" -> println("profile sent to manual review")
+                    "api" -> println("api system error")
+                }
+            }
         }
     }
 }
